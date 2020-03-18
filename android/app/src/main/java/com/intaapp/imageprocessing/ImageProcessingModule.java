b93920c20ee5a471aafd3e0de01cb58cafaed365
mmassetti <matiasmassetti@gmail.com>
2020-03-17 21:01:53 -0300

    working with image in opencv module

--- android/app/src/main/java/com/intaapp/imageprocessing/ImageProcessingModule.java
+++ android/app/src/main/java/com/intaapp/imageprocessing/ImageProcessingModule.java
@@ -1,12 +1,30 @@
 package com.intaapp.imageprocessing;
 
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+
+import org.opencv.android.Utils;
 import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import android.util.Base64;
 
 public class ImageProcessingModule extends ReactContextBaseJavaModule {
 
@@ -24,8 +42,33 @@ public class ImageProcessingModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void test(Promise promise) {
-        Core.useIPP();
-        promise.resolve("Good");
+    public void processImage(String fileUri, Promise promise) {
+
+
+        try {
+            Bitmap img = Bitmap.createBitmap(MediaStore.Images.Media.getBitmap(getReactApplicationContext().getContentResolver(), Uri.parse(fileUri)));
+            Mat mat= new Mat(img.getHeight(),img.getWidth(), CvType.CV_8UC4);
+            Utils.bitmapToMat(img,mat);
+
+            Mat result = new Mat(img.getHeight(),img.getWidth(), CvType.CV_8UC1);
+            Imgproc.cvtColor(mat,result,Imgproc.COLOR_RGB2GRAY);
+
+            //Conversion to Base64
+            Bitmap resultImg= img;
+            Utils.matToBitmap(result,resultImg);
+
+            ByteArrayOutputStream imgBase64 = new ByteArrayOutputStream();
+            resultImg.compress(Bitmap.CompressFormat.PNG,100,imgBase64);
+
+            byte[] array = imgBase64.toByteArray();
+
+            String str = Base64.encodeToString(array,Base64.DEFAULT);
+            promise.resolve(str);
+            
+        } catch (IOException e) {
+            promise.reject("OpenCv","Cannot read processed image from memory");
+            e.printStackTrace();
+        }
+
     }
 }
