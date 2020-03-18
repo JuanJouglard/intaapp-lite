8e854cdd8fbb320f893fdb8f9f2a445862ab3303
Juan Jouglard <jouglardjuan@gmail.com>
2020-03-18 16:49:02 -0300

    Added image view

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {PureComponent} from 'react';
-import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
+import {Image, StyleSheet, View, Text, TouchableOpacity} from 'react-native';
 
 import {NativeModules} from 'react-native';
 import {RNCamera} from 'react-native-camera';
@@ -7,34 +7,61 @@ import {RNCamera} from 'react-native-camera';
 class App extends PureComponent {
   camera;
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      base64: null,
+    };
+  }
+
   takePicture = async () => {
     if (this.camera) {
-      const options = {quality: 0.5, base64: true};
+      const options = {
+        quality: 0.5,
+        width: 1920,
+        base64: true,
+        fixOrientation: true,
+        pauseAfterCapture: true,
+      };
       const data = await this.camera.takePictureAsync(options);
-      msg = NativeModules.NativeOpenCV.processImage(data.uri).then(console.log);
+      const msg = await NativeModules.NativeOpenCV.processImage(data.uri);
+      this.setState({base64: msg});
     }
   };
 
   render() {
-    return (
-      <>
-        <RNCamera
-          ref={ref => {
-            this.camera = ref;
-          }}
-          style={styles.preview}
-          type={RNCamera.Constants.Type.back}
-          flashMode={RNCamera.Constants.FlashMode.on}
-        />
-        <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center'}}>
-          <TouchableOpacity
-            onPress={this.takePicture.bind(this)}
-            style={styles.capture}>
-            <Text style={{fontSize: 14}}> SNAP </Text>
-          </TouchableOpacity>
+    if (this.state.base64) {
+      return (
+        <View style={{flex: 1}}>
+          <Image
+            style={{flex: 1, borderColor: 'black', borderWidth: 5, margin: 10}}
+            source={{uri: `data:image/png;base64,${this.state.base64}`}}
+          />
         </View>
-      </>
-    );
+      );
+    } else {
+      return (
+        <>
+          <RNCamera
+            autoFocus={RNCamera.Constants.AutoFocus.on}
+            captureAudio={false}
+            ref={ref => {
+              this.camera = ref;
+            }}
+            style={styles.preview}
+            type={RNCamera.Constants.Type.back}
+          />
+          <View
+            style={{flex: 0, flexDirection: 'row', justifyContent: 'center'}}>
+            <TouchableOpacity
+              onPress={this.takePicture.bind(this)}
+              style={styles.capture}>
+              <Text style={{fontSize: 14}}> SNAP </Text>
+            </TouchableOpacity>
+          </View>
+        </>
+      );
+    }
   }
 }
 
