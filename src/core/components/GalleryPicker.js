c6f795874fab4f90a214d98a23702847918d35a5
mmassetti <matiasmassetti@gmail.com>
2020-03-19 16:49:32 -0300

    trying image picker library

--- src/core/components/GalleryPicker.js
+++ src/core/components/GalleryPicker.js
@@ -1,9 +1,180 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
+import ImagePicker from 'react-native-image-picker';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Image,
+  Button,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
+
+import {NativeModules} from 'react-native';
 
 export class GalleryPicker extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-    constructor(props) {
-        super(props);
-    }
-    
+  launchCamera = () => {
+    let options = {
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+    ImagePicker.launchCamera(options, async response => {
+      // console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+        //alert(response.customButton);
+      } else {
+        const source = {uri: response.uri};
+        //console.log('response', JSON.stringify(response));
+        this.setState({
+          filePath: response,
+          fileData: response.data,
+          fileUri: response.uri,
+        });
+        console.log(
+          'GalleryPicker -> launchCamera -> response.uri',
+          response.uri,
+        );
+        const msg = await NativeModules.NativeOpenCV.processImage(response.uri);
+
+        console.log('GalleryPicker -> launchCamera -> const msg', msg);
+      }
+    });
+  };
+
+  launchImageLibrary = () => {
+    let options = {
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+    ImagePicker.launchImageLibrary(options, async response => {
+      //console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+        //alert(response.customButton);
+      } else {
+        const source = {uri: response.uri};
+        console.log('response', JSON.stringify(response));
+        this.setState({
+          filePath: response,
+          fileData: response.data,
+          fileUri: response.uri,
+        });
+        const msg = await NativeModules.NativeOpenCV.processImage(response.uri);
+
+        console.log('GalleryPicker -> launchCamera -> const msg', msg);
+      }
+    });
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <View style={styles.body}>
+            <Text
+              style={{textAlign: 'center', fontSize: 20, paddingBottom: 10}}>
+              Pick Images from Camera & Gallery
+            </Text>
+            {/* <View style={styles.ImageSections}>
+              <View>
+                {this.renderFileData()}
+                <Text  style={{textAlign:'center'}}>Base 64 String</Text>
+              </View>
+              <View>
+                {this.renderFileUri()}
+                <Text style={{textAlign:'center'}}>File Uri</Text>
+              </View>
+            </View> */}
+
+            <View style={styles.btnParentSection}>
+              <TouchableOpacity
+                onPress={this.launchCamera}
+                style={styles.btnSection}>
+                <Text style={styles.btnText}>Directly Launch Camera</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                onPress={this.launchImageLibrary}
+                style={styles.btnSection}>
+                <Text style={styles.btnText}>
+                  Directly Launch Image Library
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </SafeAreaView>
+      </Fragment>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: 'white',
+  },
+
+  body: {
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    borderColor: 'black',
+    borderWidth: 1,
+    height: Dimensions.get('screen').height - 20,
+    width: Dimensions.get('screen').width,
+  },
+  ImageSections: {
+    display: 'flex',
+    flexDirection: 'row',
+    paddingHorizontal: 8,
+    paddingVertical: 8,
+    justifyContent: 'center',
+  },
+  images: {
+    width: 150,
+    height: 150,
+    borderColor: 'black',
+    borderWidth: 1,
+    marginHorizontal: 3,
+  },
+  btnParentSection: {
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  btnSection: {
+    width: 225,
+    height: 50,
+    backgroundColor: '#DCDCDC',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 3,
+    marginBottom: 10,
+  },
+  btnText: {
+    textAlign: 'center',
+    color: 'gray',
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+});
