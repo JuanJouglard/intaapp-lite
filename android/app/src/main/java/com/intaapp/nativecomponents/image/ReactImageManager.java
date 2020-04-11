510224a060b8a8ff851e6107fee3949e75c3d5bd
Juan Jouglard <jouglardjuan@gmail.com>
2020-04-11 17:30:27 -0300

    Save image

--- android/app/src/main/java/com/intaapp/nativecomponents/image/ReactImageManager.java
+++ android/app/src/main/java/com/intaapp/nativecomponents/image/ReactImageManager.java
@@ -1,5 +1,9 @@
 package com.intaapp.nativecomponents.image;
 
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.ColorFilter;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
@@ -13,14 +17,21 @@ import androidx.annotation.Nullable;
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.common.MapBuilder;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.ViewProps;
 import com.facebook.react.uimanager.annotations.ReactProp;
 import com.facebook.react.views.image.ImageResizeMode;
 import com.facebook.react.views.image.ReactImageView;
+import com.intaapp.utilities.ImageSaver;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 public class ReactImageManager extends SimpleViewManager<ReactImageView> {
 
@@ -28,12 +39,14 @@ public class ReactImageManager extends SimpleViewManager<ReactImageView> {
     float currentBright;
     float currentSat;
     float currentContrast;
+    ImageSaver imgSaver;
 
     public ReactImageManager(ReactApplicationContext context) {
         mCallerContext = context;
         currentBright = 0;
         currentContrast = 1;
         currentSat = 1;
+        imgSaver = new ImageSaver(context);
     }
 
     @NonNull
@@ -79,7 +92,16 @@ public class ReactImageManager extends SimpleViewManager<ReactImageView> {
     @ReactProp(name = "saveImage")
     public void saveImage(ReactImageView view, @Nullable boolean save) {
         Log.i("SAVEIMG", save+"");
+        if (save) {
+            Bitmap bmp = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(bmp);
+            view.draw(canvas);
+            imgSaver.setFileName("modified.png").setDirectoryName("intaApp").save(bmp);
+            Bitmap bit = imgSaver.setFileName("modified.png").setDirectoryName("intaApp").load();
+            Log.i("BITMAPON", bit.toString());
+        }
     }
+
 
     private ColorMatrixColorFilter applyFilter(float brightness,float contrast,float saturation) {
         ColorMatrix satMatrix = new ColorMatrix();
