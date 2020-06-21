0e3c6c7d9f31cdcf412450e5320b0341da6467b6
mmassetti <matiasmassetti@gmail.com>
2020-06-21 17:57:54 -0300

    remove unused code

--- src/configuration/colors.js
+++ src/configuration/colors.js
@@ -8,14 +8,6 @@ const upperYellow = [35, 255, 255];
 
 export const yellowRange = [lowerYellow, upperYellow];
 
-const lowerRed1 = [0];
-const upperRed1 = [10];
-const lowerRed2 = [170];
-const upperRed2 = [179];
-
-//export const redRange1 = [lowerRed1,upperRed1];
-//export const redRange2 = [lowerRed2,upperRed2];
-
 export const mainThemeColor = opacity => `rgba(31, 47, 51, ${opacity})`;
 
 export const accentThemeColor = opacity => `rgba(31, 47, 51, ${opacity})`;
